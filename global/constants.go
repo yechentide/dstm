edd7a4aa4eee7794562a2775a820cd96d9a2b041
yechentide <yechentide@gmail.com>
2024-03-21 20:18:00 +0900

    refac: merge constants files

--- global/constants.go
+++ global/constants.go
@@ -1,6 +1,10 @@
 package global
 
 const (
+	FILE_PATH_PKG_INSTALL_CHECK string = "/tmp/is_installed.txt"
+	DIR_PATH_EXTRACTOR_SCRIPT   string = "/tmp/dst-extractor-script"
+	DIR_PATH_EXTRACT_WORKSPACE  string = "/tmp/dst-extract"
+
 	SESSION_STEAM_INSTALL string = "dstm-install-steam"
 	SESSION_DST_INSTALL   string = "dstm-install-dst"
 
